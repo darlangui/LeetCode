bd79d3a73867a61504228aeb004e3ee8f7a6fcc6
Darlan <darlaneduardoguimaraes@gmail.com>
2023-05-24 13:35:22 -0300

    347

---
+++ 14-longestCommonPrefix.php
@@ -0,0 +1,20 @@
+<?php
+class Solution {
+
+    /**
+     * @param String[] $strs
+     * @return String
+     */
+    function longestCommonPrefix($strs) {
+        $result = $strs[0];
+        for ($i = 1; $i < count($strs); $i++) {
+            $str = $strs[$i];
+            while (substr($str, 0, strlen($result)) != $result && strlen($result) > 0)
+                $result = substr($result, 0, strlen($result) - 1);
+        }
+        return $result;
+    }
+}
+
+$test = new Solution();
+echo $test->longestCommonPrefix(["flower","flow","flight"]);
