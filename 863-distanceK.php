b58355dfb4bbf137c037a378f0791c97cdea536a
Darlan <darlaneduardoguimaraes@gmail.com>
2023-07-11 08:56:55 -0300

    863

---
+++ 863-distanceK.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @param TreeNode $root
+ * @param TreeNode $target
+ * @param Integer $k
+ * @return Integer[]
+ */
+function distanceK($root, $target, $k) {
+    var_dump($target);
+    return [];
+}
+
